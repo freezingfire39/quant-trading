d566bdc8084bd37e2e8da8752b3dd29064026706
blesssecret <wenyeyu@usc.edu>
2020-09-11 02:40:06 -0700

    Update cornk_final.py

--- jt/pattern_matching_algo/cornk_final.py
+++ jt/pattern_matching_algo/cornk_final.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import sys
 import pickle
 import optuna
 import matplotlib.pyplot as plt
@@ -11,7 +12,11 @@ pio.renderers.default = "svg" # Toggle for GitHub rendering
 
 from mlfinlab.online_portfolio_selection import *
 
-data = pd.read_csv('sp_500.csv', parse_dates=True, index_col='date')
+# input_path = 'sp_500.csv'
+input_path = sys.argv[1]
+output_path = sys.argv[2]
+
+data = pd.read_csv(input_path, parse_dates=True, index_col='date')
 data = data.drop(data.columns[50:400],axis=1)
 data.dropna(inplace=True)
 
@@ -23,4 +28,4 @@ equity_cornk = optuna.load_study(study_name='cornk', storage='sqlite:///TestDB.d
 equity_cornk_ = CORNK(window=equity_cornk.best_params['window'], rho=equity_cornk.best_params['rho'],k=equity_cornk.best_params['k'])
 equity_cornk_.allocate(data)
 positions = equity_cornk_.all_weights
-positions.to_csv ('weights_cornk.csv', header=True)
+positions.to_csv (output_path, header=True)
