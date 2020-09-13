fa66fd4af6c913857a7de68bed0b75da0301802a
blesssecret <wenyeyu@usc.edu>
2020-09-13 01:01:17 -0700

    Update cornk_final.py

--- jt/pattern_matching_algo/cornk_final.py
+++ jt/pattern_matching_algo/cornk_final.py
@@ -15,6 +15,7 @@ from mlfinlab.online_portfolio_selection import *
 # input_path = 'sp_500.csv'
 input_path = sys.argv[1]
 output_path = sys.argv[2]
+testDB_path = sys.argv[3]
 
 data = pd.read_csv(input_path, parse_dates=True, index_col='date')
 data = data.drop(data.columns[50:400],axis=1)
@@ -24,7 +25,8 @@ with open('pickled/equity_cornk.pickle', 'rb') as fout:
     equity_cornk = pickle.load(fout)
 equity_cornk = pd.DataFrame(equity_cornk, columns=['Window','Rho','K','Returns']).sort_values('Returns', ascending=False)
 
-equity_cornk = optuna.load_study(study_name='cornk', storage='sqlite:///TestDB.db')
+# equity_cornk = optuna.load_study(study_name='cornk', storage='sqlite:///TestDB.db')
+equity_cornk = optuna.load_study(study_name='cornk', storage='sqlite://' + testDB_path)
 equity_cornk_ = CORNK(window=equity_cornk.best_params['window'], rho=equity_cornk.best_params['rho'],k=equity_cornk.best_params['k'])
 equity_cornk_.allocate(data)
 positions = equity_cornk_.all_weights
